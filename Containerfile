867f2b87a6fa27007941ec5fbe2fee3f3a46319a
Dom Rodriguez <shymega@shymega.org.uk>
2025-10-19 00:43:06 +0100

    feat: Add ZFS support

--- Containerfile
+++ Containerfile
@@ -1,14 +1,19 @@
 FROM scratch AS ctx
 COPY build_files /
 
+# ZFS kernel and module
+FROM ghcr.io/ublue-os/akmods-zfs:coreos-testing-42 AS zfs-cache
+
 FROM ghcr.io/ublue-os/bazzite:42@sha256:43b0a405fb12476ed3486cfe9f005b412bceea00fad9978e9f95496e30677ec8 as amyos
 COPY system_files /
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-  --mount=type=cache,dst=/var/cache \
-  --mount=type=cache,dst=/var/log \
-  --mount=type=tmpfs,dst=/tmp \
-  /ctx/install-apps.sh && \
-  /ctx/fix-opt.sh && \
-  /ctx/build-initramfs.sh && \
-  /ctx/cleanup.sh
+    --mount=type=bind,from=zfs-cache,src=/kernel-rpms,dst=/tmp/rpms/kernel \
+    --mount=type=bind,from=zfs-cache,src=/rpms/kmods/zfs,dst=/tmp/rpms/zfs \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/install-apps.sh && \
+    /ctx/fix-opt.sh && \
+    /ctx/build-initramfs.sh && \
+    /ctx/cleanup.sh
--- build_files/install-apps.sh
+++ build_files/install-apps.sh
@@ -7,6 +7,26 @@ log() {
   echo "=== $* ==="
 }
 
+## ZFS (originally derived from https://github.com/shymega/shyBazzite-zfs-test/blob/main/build_files/build.sh)
+
+RELEASE="$(rpm -E %fedora)"
+
+# as per https://openzfs.github.io/openzfs-docs/Getting%20Started/Fedora/index.html
+# "zfs-fuse is unmaintained and should not be used under any circumstance"
+rpm -e --nodeps zfs-fuse
+dnf install -y https://zfsonlinux.org/fedora/zfs-release-2-8$(rpm --eval "%{dist}").noarch.rpm
+dnf install -y kernel-devel-"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+dnf install -y zfs
+
+# Auto-load ZFS module
+depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" && \
+echo "zfs" > /etc/modules-load.d/zfs.conf && \
+# we don't want any files on /var
+rm -rf /var/lib/pcp
+## Just in case, according to https://openzfs.github.io/openzfs-docs/Getting%20Started/Fedora/index.html#installation
+echo 'zfs' > /etc/dnf/protected.d/zfs.conf
+
+
 # Workaround to allow installing nix with composefs
 mkdir /nix
 
