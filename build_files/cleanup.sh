bea4ba1a3cb513511471213c4a5c8fe9fcfd7b21
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-06 20:48:25 +0000

    various modifications

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -19,7 +19,7 @@ dnf5 clean all
 # rm -rf /tmp/*
 
 # Clean /var directory while preserving essential files
-find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
+# find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
 
 # Restore and setup directories
